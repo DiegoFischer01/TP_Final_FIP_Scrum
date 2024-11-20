9a11426ba5481d7894f3d8c32e49da5d20f8e67b
Diego Fischer <diegof01gm@gmail.com>
2024-11-20 18:18:05 -0300

    actualice main

--- VETERINARIA/main.ts
+++ VETERINARIA/main.ts
@@ -6,13 +6,16 @@ const clienteJuan = new Cliente("Juan", "2284-345678", [
     { nombre: "Fido", especie: "perro" },
     { nombre: "Rex", especie: "gato" }
 ]);
-
 console.log(clienteJuan);
 console.log(clienteJuan.getId());
 
 
-
-
-const Diego = new Cliente ("Diego Fischer", "2284-507340",[{nombre:"Ciri", especie:"gato"}]);
-console.log(Diego);
-console.log(Diego.getId());
+const diego = new Cliente("Diego Fischer", "2284-507340", [{ nombre: "Ciri", especie: "gato" }]);
+console.log(diego);
+console.log(diego.getId());
+diego.incrementarVisitas();
+diego.incrementarVisitas();
+diego.incrementarVisitas();
+diego.incrementarVisitas();
+diego.incrementarVisitas();
+console.log(diego);
